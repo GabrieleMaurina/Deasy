29b248d820d19de27ad6ccc667922ee3cd262ce3
MrMagik <30479454+MrMagik@users.noreply.github.com>
2017-11-24 12:40:13 +0100

    fixing index.js

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var Intent = require('./intent');
+const path = require('path');
 
 // instantiate express
 var app = express();
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
 
 // instantiate mongoose
 mongoose.Promise = global.Promise;
