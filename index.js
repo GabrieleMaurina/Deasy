9a429c78fcea104835ca3bc3e3d68f2ff86a6298
Michele Armellini <armellinimichele@live.it>
2017-11-30 10:56:48 +0100

    Yet another test

--- index.js
+++ index.js
@@ -74,8 +74,7 @@ function getValues(keys, res)
 			{
 				params[paramKeys[i]] = values[i]; 
 			}
-			res.send(JSON.stringify({"contextOut":[{"name":"webhook_dialog_context","parameters":params}]}));
-			res.send(JSON.stringify({"followupEvent":{"name":"webhook","data":{params}}}));
+			res.send(JSON.stringify({"contextOut":[{"name":"webhook_dialog_context","parameters":params}],"followupEvent":{"name":"webhook","data":{params}}}));
 			console.log(JSON.stringify({"contextOut":[{"name":"webhook","parameters":params}]}));
 		});
 	});
