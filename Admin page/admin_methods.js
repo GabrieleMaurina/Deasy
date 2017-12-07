aa77e9d1a9adf54753a57336d34334fdf5451db6
EnricoGdx <Enrico.Gdx@gmail.com>
2017-12-07 16:34:32 +0100

    changed into delete request,added tag methods and file upload

--- Admin page/admin_methods.js
+++ Admin page/admin_methods.js
@@ -1,64 +1,134 @@
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ made by EnricoGdx
  */
 
 
 
-var xhttp, url;
+var xhttp, url = "/admin.php", admin = false;
 
 function authentication() {
-    var admin = false;
     var usrnm = document.getElementById("username").value;
     var psswrd = document.getElementById("password").value;
 
-    /*xhttp = new XMLHttpRequest();
-     xhttp.onreadystatechange = function () {
-     if (this.readyState == 4 && this.status == 200) {
-     var accepted = xhttp.responseText;
-     if (accepted === "ok")
-     admin = true;
-     tabes();
-     else
-     document.getElementById("not_accepted").style.visibility = "visible";
-     }
-     };
-     xhttp.open("POST", url, true);
-     xhttp.send(usrnm + " " + psswrd);*/ admin = true;
-    tables(admin);
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var accepted = xhttp.responseText;
+            if (accepted === "ok") {
+                admin = true;
+                document.getElementById("tag_or_field").style.visibility = "visible";
+            } else
+                document.getElementById("not_accepted").style.visibility = "visible";
+        }
+    };
+    xhttp.open("POST", url, true);
+    xhttp.send(usrnm + " " + psswrd);/* admin = true;
+    document.getElementById("tag_or_field").style.visibility = "visible";*/
+
 }
 
-function tables(admin) {
+function field_or_tag() {
+
+    var choice = document.getElementById("tag_or_field").value;
+    if (choice === "tag") {
+        document.getElementById("modify_tag").style.visibility = "visible";
+        document.getElementById("modify_field").style.visibility = "hidden";
+
+    } else if (choice === "field") {
+        document.getElementById("modify_field").style.visibility = "visible";
+        document.getElementById("modify_tag").style.visibility = "hidden";
+
+    } else {
+        document.getElementById("modify_field").style.visibility = "hidden";
+        document.getElementById("modify_tag").style.visibility = "hidden";
+    }
+}
+
+function table_field() {
 
     if (admin) {
         xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function () {
             if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("modify_table").style.visibility = "visible";
                 document.getElementById("tabella").innerHTML = xhttp.responseText;
             }
         };
-        xhttp.open("GET", "http://localhost:8084/IngSoft2_prog/admin.php", true);
+        xhttp.open("GET", url, true);
         xhttp.send();
     } else
         document.getElementById("not_accepted").style.visibility = "visible";
 }
 
-function modify() {
+function modify_field() {
 
-    var operation = document.getElementById("operation").value;
+    var operation = document.getElementById("operation_field").value;
     var field_name = document.getElementById("field_name").value;
-    var answer = document.getElementById("answer").value;
+    var answer = document.getElementById("field_answer").value;
     if (operation !== "") {
         xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function () {
             if (this.readyState == 4 && this.status == 200) {
-                tables(true);
+                table_field();
             }
         };
-        xhttp.open("POST", url, true);
+        if (operation === "remove") {
+            xhttp.open("DELETE", url, true);
+        } else
+            xhttp.open("POST", url, true);
         xhttp.send(operation + " " + field_name + " " + answer);
     }
 }
+
+function table_tag() {
+
+    if (admin) {
+        xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("tabella").innerHTML = xhttp.responseText;
+            }
+        };
+        xhttp.open("GET", url, true);
+        xhttp.send();
+    } else
+        document.getElementById("not_accepted").style.visibility = "visible";
+}
+
+function modify_tag() {
+
+    var operation = document.getElementById("operation_tag").value;
+    var tag_name = document.getElementById("tag_name").value;
+    var answer_function = document.getElementById("tag_function").value;
+    var fileInput = document.getElementById('fileInput');
+    var file = fileInput.files[0];
+    var file_in_json = "";
+    var string;
+    var reader = new FileReader();
+
+    string = reader.readAsText(file);
+    file_in_json = JSON.stringify(string);
+
+
+    if (operation === "remove") {
+
+        xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                table_field();
+            }
+        };
+        xhttp.open("DELETE", url, true);
+        xhttp.send(operation + " " + tag_name);
+    } else if (operation === "add") {
+        xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                table_field();
+            }
+        };
+        xhttp.open("POST", url, true);
+        xhttp.send(operation + " " + tag_name + answer_function + file_in_json);
+    }
+}
+
 
