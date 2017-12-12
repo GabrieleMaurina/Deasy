81c5c06a67c48c6abfa8f9ad3e004f8d80b2957b
EnricoGdx <Enrico.Gdx@gmail.com>
2017-12-12 22:22:14 +0100

    test on bug at authentication

--- dist/admin_methods.js
+++ dist/admin_methods.js
@@ -16,15 +16,12 @@ function authentication() {
         document.getElementById("parameters_or_intent").style.visibility = "visible";
     } else
         document.getElementById("not_accepted").style.visibility = "visible";
-        document.getElementById("parameters_or_intent").style.visibility = "hidden";
+        /*document.getElementById("parameters_or_intent").style.visibility = "hidden";
         document.getElementById("modify_intent").style.visibility = "hidden";
         document.getElementById("modify_parameters").style.visibility = "hidden";
-        document.getElementById("tabella").innerHTML = "";
-        //admin = false;
+        document.getElementById("tabella").innerHTML = "";*/
+        admin = false;
 }
-/* admin = true;
- document.getElementById("parameters_or_intent").style.visibility = "visible";*/
-
 
 
 function intent_or_parameters() {
