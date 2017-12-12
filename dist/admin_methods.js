a5602b6a309faaba410fafb2a8e2708d4c610473
EnricoGdx <Enrico.Gdx@gmail.com>
2017-12-12 21:46:57 +0100

    working code with no upload for json files

--- dist/admin_methods.js
+++ dist/admin_methods.js
@@ -10,7 +10,7 @@ function authentication() {
     var usrnm = document.getElementById("username").value;
     var psswrd = document.getElementById("password").value;
 
-    if (usrnm === "admin_deasy" && psswrd === "123") {
+    if (usrnm === "easy_name" && psswrd === "easy_password") {
         admin = true;
         document.getElementById("not_accepted").style.visibility = "hidden";
         document.getElementById("parameters_or_intent").style.visibility = "visible";
@@ -25,31 +25,42 @@ function authentication() {
 function intent_or_parameters() {
 
     var choice = document.getElementById("parameters_or_intent").value;
-    var elements;
+    
     if (choice === "parameters") {
         document.getElementById("modify_parameters").style.visibility = "visible";
         document.getElementById("modify_intent").style.visibility = "hidden";
+        table(urlpar);
 
     } else if (choice === "intent") {
         document.getElementById("modify_intent").style.visibility = "visible";
         document.getElementById("modify_parameters").style.visibility = "hidden";
+        table(urlint);
 
     } else {
         document.getElementById("modify_intent").style.visibility = "hidden";
         document.getElementById("modify_parameters").style.visibility = "hidden";
+        document.getElementById("tabella").innerHTML = "";
     }
 }
 
-function table_intent() {
+function table(url) {
 
+    var elements;
     if (admin) {
         xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function () {
             if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("tabella").innerHTML = xhttp.responseText;
+                elements = JSON.parse(xhttp.responseText);
+                var table = "<table>";
+                for (i = 0; i < elements.length; i++) {
+                    console.log();
+                    table += "<tr><td>" + elements[i].key + "</td><td>" + elements[i].value + "</td></tr>";
+                }
+                table += "</table>";
+                document.getElementById("tabella").innerHTML = table;
             }
         };
-        xhttp.open("GET", urlint, true);
+        xhttp.open("GET", url, true);
 
         xhttp.send()
     } else
@@ -61,42 +72,28 @@ function modify_intent() {
     var operation = document.getElementById("operation_intent").value;
     var intent_name = document.getElementById("intent_name").value;
     var intent_answer = document.getElementById("intent_answer").value;
-    var answer = [], string;
+    var answer = [];
 
     if (operation !== "") {
         xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function () {
             if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("tabella").innerHTML = xhttp.responseText;
+                table(urlint);
             }
         };
         if (operation === "remove") {
             xhttp.open("DELETE", urlint, true);
-
             answer.push(intent_name);
 
         } else {
             xhttp.open("POST", urlint, true);
             answer.push({key: intent_name, value: intent_answer});
         }
+
+        xhttp.setRequestHeader("Content-type", "Application/json");
         json_answer = JSON.stringify(answer);
         xhttp.send(json_answer);
     }
-}
-
-function table_parameters() {
-
-    if (admin) {
-        xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("tabella").innerHTML = xhttp.responseText;
-            }
-        };
-        xhttp.open("GET", urlpar, true);
-        xhttp.send();
-    } else
-        document.getElementById("not_accepted").style.visibility = "visible";
 }
 
 function modify_parameters() {
@@ -104,61 +101,52 @@ function modify_parameters() {
     var operation = document.getElementById("operation_parameters").value;
     var parameters_name = document.getElementById("parameters_name").value;
     var parameters_answer = document.getElementById("parameters_function").value;
-    var fileInput = document.getElementById('fileInput');
-    var file = fileInput.files[0];
-    var file_in_json = "", string = "";
-    var reader = new FileReader(), error = false;
-    var answer = [];
+    var answer = []; 
+     /*var error = false;
+     var fileInput = document.getElementById('fileInput');     da corregere il bug
+     var file = fileInput.files[0];
+     var file_in_json = "", string = "";
+     var reader = new FileReader();
+     
+     var file_exists = (fileInput.files.length > 0);
+     if (file_exists) {
+     string = reader.readAsText(file);
+     file_in_json = JSON.stringify(string);
+     }
+     
+     if (file_exists && parameters_name !== "" && operation !== "remove") {
+     document.getElementById("errore_parameters").style.visibility = "visible";
+     error = true;
+     }*/
 
-    var file_exists = (fileInput.files.length > 0);
-    if (file_exists) {
-        string = reader.readAsText(file);
-        file_in_json = JSON.stringify(string);
-    }
-
-    if (file_exists && parameters_name !== "" && operation !== "remove") {
-        document.getElementById("errore_parameters").style.visibility = "visible";
-        error = true;
-    }
-
-    if (!error) {
-        xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                elements = JSON.parse(xhttp.responseText);
-                var table = "<table>";
-                for(i = 0; i < elements.length; i++){
-                    console.log();
-                    table += "<tr><td>"+elements[i].key+"</td><td>"+elements[i].value+"</td></tr>";
-                }
-                table += "</table>";
-                document.getElementById("tabella").innerHTML = table;
-            }
-        };
-
-        if (operation === "remove") {
-            xhttp.open("DELETE", urlpar, true);
-            xhttp.setRequestHeader("Content-type","Application/json");
-            answer.push(parameters_name);
-
-        } else {
-            if (parameters_name !== "") {
-                document.getElementById("errore_parameters").style.visibility = "hidden";
-                xhttp.open("POST", urlpar, true);
-                xhttp.setRequestHeader("Content-type","Application/json")
-                answer.push({key : parameters_name, value : parameters_answer});
-            } else {
-                document.getElementById("errore_parameters").style.visibility = "hidden";
-                xhttp.open("POST", urlpar, true);
-                xhttp.setRequestHeader("Content-type","Application/json")
-                answer.push({key : + parameters_name, value : file_in_json});
-            }
+    // if (!error) {
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+           table(urlpar);
         }
-        console.log(answer);
-        json_answer = JSON.stringify(answer);
-        console.log(json_answer);
-        xhttp.send(json_answer);
+    };
+
+    if (operation === "remove") {
+        xhttp.open("DELETE", urlpar, true);
+        answer.push(parameters_name);
+
+    } else {
+        if (parameters_name !== "") {
+            document.getElementById("errore_parameters").style.visibility = "hidden";
+            xhttp.open("POST", urlpar, true);
+            answer.push({key: parameters_name, value: parameters_answer});
+        } /*else {
+         document.getElementById("errore_parameters").style.visibility = "hidden";
+         xhttp.open("POST", urlpar, true);
+         answer.push({key: +parameters_name, value: file_in_json});
+         }*/
     }
+
+    xhttp.setRequestHeader("Content-type", "Application/json");
+    json_answer = JSON.stringify(answer);
+    xhttp.send(json_answer);
+    // }
 }
 
 
