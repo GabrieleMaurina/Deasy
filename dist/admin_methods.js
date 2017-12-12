0c4ee103f1e2dd6e7f09839fc9b905111e5b51ce
EnricoGdx <Enrico.Gdx@gmail.com>
2017-12-12 22:10:22 +0100

    solved bug at authentication

--- dist/admin_methods.js
+++ dist/admin_methods.js
@@ -16,6 +16,10 @@ function authentication() {
         document.getElementById("parameters_or_intent").style.visibility = "visible";
     } else
         document.getElementById("not_accepted").style.visibility = "visible";
+        document.getElementById("parameters_or_intent").style.visibility = "hidden";
+        document.getElementById("modify_intent").style.visibility = "hidden";
+        document.getElementById("modify_parameters").style.visibility = "hidden";
+        document.getElementById("tabella").innerHTML = "";
         admin = false;
 }
 /* admin = true;
